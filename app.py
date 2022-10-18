dd0589198809f03547932389224e1cd2ec6fd117
Daniel <daniel.clements14@gmail.com>
2022-10-18 15:37:39 +0200

    added a form on webpage load to enter the link to a property, pass that link to beautiful soup that then gathers data from the link

--- app.py
+++ app.py
@@ -1,91 +1,89 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from bs4 import BeautifulSoup
 import requests
 
 app = Flask(__name__)
 
 
-# BeautifulSoup
-headers = {
-    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.12; rv:55.0) Gecko/20100101 Firefox/55.0',
-    }
-html_text = requests.get('https://infinitycasas.com/properties/fmc007-2-bedroom-town-house-for-sale-in-la-zenia/', headers=headers).text
-soup = BeautifulSoup(html_text, 'lxml')
 
-# gets all images in the image gallery
-all_property_images = soup.find_all('a', class_='galleryItem')
-links_array = []
-for link in all_property_images:
-    source = link.attrs['href']
-    links_array.append(source)
+@app.route('/', methods=['POST', "GET"])
+def index():
+    if request.method == "POST":
+        link_main = request.form["property-link"]
+         # BeautifulSoup
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.12; rv:55.0) Gecko/20100101 Firefox/55.0',
+            }
+        html_text = requests.get(link_main, headers=headers).text
+        soup = BeautifulSoup(html_text, 'lxml')
 
-mainImage = links_array[0]
-image_top_row_r1 = links_array[-2]
-image_top_row_r2 = links_array[4]
-image_bottom_row_r1 = links_array[10]
-image_bottom_row_r2 = links_array[9]
-image_bottom_row_r3 = links_array[5]
+        # gets all images in the image gallery
+        all_property_images = soup.find_all('a', class_='galleryItem')
+        links_array = []
+        for link in all_property_images:
+            source = link.attrs['href']
+            links_array.append(source)
 
-
-# get property Header
-property_header = soup.find('h1', class_='pageTitle').text
-
-
-# get property Ref
-
-property_ref = soup.find('div', class_='additional').find_all('div', class_='amItem')[-1].text.split()[-1]
-
-# get property Features
-
-property_features=soup.find('div', class_='amenities').find_all('div', class_='amItem')
-feature_array = []
-pool = "Communal Pool"
+        mainImage = links_array[0]
+        image_top_row_r1 = links_array[-2]
+        image_top_row_r2 = links_array[4]
+        image_bottom_row_r1 = links_array[10]
+        image_bottom_row_r2 = links_array[9]
+        image_bottom_row_r3 = links_array[5]
 
 
-# get property_description
-
-# property_description = soup.find('div', class_='entry-content').find('ul').find_all('li')
-property_description = soup.find('div', class_='entry-content').find_all('p')
-property_array = []
-
-for item in property_description:
-    property_array.append(item.text)
+        # get property Header
+        property_header = soup.find('h1', class_='pageTitle').text
 
 
+        # get property Ref
+
+        property_ref = soup.find('div', class_='additional').find_all('div', class_='amItem')[-1].text.split()[-1]
+
+        # get property Features
+
+        property_features=soup.find('div', class_='amenities').find_all('div', class_='amItem')
+        feature_array = []
+        pool = "Communal Pool"
 
 
+        # get property_description
 
-# get bedrooms oand bathrooms
+        # property_description = soup.find('div', class_='entry-content').find('ul').find_all('li')
+        property_description = soup.find('div', class_='entry-content').find_all('p')
+        property_array = []
 
-beds = soup.find('ul', class_='features').find_all('li')[0].find('div').text[0]
-baths = soup.find('ul', class_='features').find_all('li')[1].find('div').text[0]
-
-
-# Get property price
-
-price = soup.find('div', class_='listPrice').find(text=True, recursive=False)
+        for item in property_description:
+            property_array.append(item.text)
 
 
 
 
 
-@app.route('/')
-def intial_load():
-    
-    
-    return render_template(
-        'index.html', property_header=property_header, price=price, beds=beds,
-         baths=baths, pool=pool, property_ref=property_ref, mainImage=mainImage,
-         image_top_row_r1=image_top_row_r1, image_top_row_r2=image_top_row_r2,
-         image_bottom_row_r1=image_bottom_row_r1,
-         image_bottom_row_r2=image_bottom_row_r2,
-         image_bottom_row_r3=image_bottom_row_r3, property_array=property_array[0:9],
-         links_array=links_array)
+        # get bedrooms oand bathrooms
+
+        beds = soup.find('ul', class_='features').find_all('li')[0].find('div').text[0]
+        baths = soup.find('ul', class_='features').find_all('li')[1].find('div').text[0]
+
+
+        # Get property price
+
+        price = soup.find('div', class_='listPrice').find(text=True, recursive=False)
+
+        
+        return render_template(
+            'print.html', property_header=property_header, price=price, beds=beds,
+            baths=baths, pool=pool, property_ref=property_ref, mainImage=mainImage,
+            image_top_row_r1=image_top_row_r1, image_top_row_r2=image_top_row_r2,
+            image_bottom_row_r1=image_bottom_row_r1,
+            image_bottom_row_r2=image_bottom_row_r2,
+            image_bottom_row_r3=image_bottom_row_r3, property_array=property_array[0:9],
+            links_array=links_array)
+    else:
+        return render_template(
+        'index.html')
 
 
 if __name__ == "__main__":
     app.run(debug=True)
-
-
-
 
