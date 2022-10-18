3d7defde0096de33a514ecbe513f95cf8030b33b
Daniel <daniel.clements14@gmail.com>
2022-10-18 12:25:18 +0200

    added ability to change main image using javascript

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 headers = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.12; rv:55.0) Gecko/20100101 Firefox/55.0',
     }
-html_text = requests.get('https://infinitycasas.com/properties/semi-detached-villa-in-cabo-roig/', headers=headers).text
+html_text = requests.get('https://infinitycasas.com/properties/fmc007-2-bedroom-town-house-for-sale-in-la-zenia/', headers=headers).text
 soup = BeautifulSoup(html_text, 'lxml')
 
 # gets all images in the image gallery
@@ -20,11 +20,11 @@ for link in all_property_images:
     links_array.append(source)
 
 mainImage = links_array[0]
-image_top_row_r1 = links_array[11]
-image_top_row_r2 = links_array[5]
-image_bottom_row_r1 = links_array[17]
+image_top_row_r1 = links_array[-2]
+image_top_row_r2 = links_array[4]
+image_bottom_row_r1 = links_array[10]
 image_bottom_row_r2 = links_array[9]
-image_bottom_row_r3 = links_array[7]
+image_bottom_row_r3 = links_array[5]
 
 
 # get property Header
@@ -44,7 +44,8 @@ pool = "Communal Pool"
 
 # get property_description
 
-property_description = soup.find('div', class_='entry-content').find('ul').find_all('li')
+# property_description = soup.find('div', class_='entry-content').find('ul').find_all('li')
+property_description = soup.find('div', class_='entry-content').find_all('p')
 property_array = []
 
 for item in property_description:
@@ -65,6 +66,9 @@ baths = soup.find('ul', class_='features').find_all('li')[1].find('div').text[0]
 price = soup.find('div', class_='listPrice').find(text=True, recursive=False)
 
 
+
+
+
 @app.route('/')
 def intial_load():
     
@@ -75,7 +79,8 @@ def intial_load():
          image_top_row_r1=image_top_row_r1, image_top_row_r2=image_top_row_r2,
          image_bottom_row_r1=image_bottom_row_r1,
          image_bottom_row_r2=image_bottom_row_r2,
-         image_bottom_row_r3=image_bottom_row_r3, property_array=property_array[0:9])
+         image_bottom_row_r3=image_bottom_row_r3, property_array=property_array[0:9],
+         links_array=links_array)
 
 
 if __name__ == "__main__":
